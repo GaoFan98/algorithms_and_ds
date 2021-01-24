3d3a25fbc0a3fcabcf637225080e36ce3da55238
GaoFan98 <musepsycho98@gmail.com>
2021-01-24 15:34:32 +0400

    nu

---
+++ DS and Algorithms in Python/random_tests.py
@@ -0,0 +1,24 @@
+# list = [1, 4, 3, 4, 5]
+#
+# i = 0
+# while i < len(list):
+#     print(list[i])
+#     i += 1
+
+# modulus comparison keyword parameter
+# print(max(-2, -65, 32, 11, key=abs))
+
+# Integer related point and character on the integer point
+# print(ord('~'))
+# print(chr(65))
+
+# return the whole value of division and it's reminder 5//3 = (1,2)
+# print(divmod(5, 3))
+
+# returns hashed version
+# print(hash('secret'))
+
+# returns unique id version
+# print(id('secret'))
+
+print([1,3,6],sep=':')
--- DS and Algorithms in Python/test.py
+++
@@ -1,7 +0,0 @@
-# list = [1, 4, 3, 4, 5]
-#
-# i = 0
-# while i < len(list):
-#     print(list[i])
-#     i += 1
-
