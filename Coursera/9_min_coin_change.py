61b7c7e16ce56719bceb869f322a4259db86f93c
GaoFan98 <musepsycho98@gmail.com>
2021-02-02 00:23:58 +0400

    id know

--- Coursera/9_min_coin_change.py
+++ Coursera/9_min_coin_change.py
@@ -1,17 +1,19 @@
 def get_change(given_coin):
-    coins = [1,5,10]
+    coins = [1, 5, 10]
 
     if given_coin == 0:
-    	return 0
+        return 0
 
-    table = [0] + [given_coin+1 for i in range(given_coin)]  
-    
-    for index in range(0, given_coin+1):
-    	for coin in coins:
-    		if index >= coin:
-    			table[index] = min(table[index],table[index-coin]+1)
+    table = [0] + [given_coin + 1 for i in range(given_coin)]
+    # print(table)
 
-    return table[index]	
+    for index in range(0, given_coin + 1):
+        for coin in coins:
+            if index >= coin:
+                table[index] = min(table[index], table[index - coin] + 1)
+
+    return table[index]
+
 
 given_coin = int(input())
 print(get_change(given_coin))
