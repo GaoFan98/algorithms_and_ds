e18a2b8bce6bc484c9500a8533d390830f29150c
GaoFan98 <musepsycho98@gmail.com>
2021-02-12 18:54:24 +0400

    fibo recursive method

--- Coursera/1_fibonacci_number.py
+++ Coursera/1_fibonacci_number.py
@@ -9,17 +9,32 @@
 # print(calc_fib(n))
 
 # Faster way of fibonacci
-def calc_fib(n):
-	arr_nums = [0,1]
+# def calc_fib(n):
+#     arr_nums = [0, 1]
+#
+#     if (n <= 1):
+#         return n
+#
+#     for i in range(2, n + 1):
+#         arr_nums.append(arr_nums[-1] + arr_nums[-2])
+#
+#     return arr_nums[-1]
+#
+#
+# print(calc_fib(9))
+#
 
-	if (n <= 1):
-		return n
-        
-	for i in range(2,n+1):
-		arr_nums.append(arr_nums[-1]+arr_nums[-2])
+# Another way of Fibonacci with O(n)
 
-	return arr_nums[-1]
-
-
-n = int(input())
-print(calc_fib(n))
+# def good_fibonacci(n):
+#     if (n <= 1):
+#         return (n, 0)
+#
+#     else:
+#         # here good_fibonacci works as counter-=1 step
+#         (a, b) = good_fibonacci(n - 1)
+#         # print(a,b)
+#         return (a + b, a)
+#
+#
+# print(good_fibonacci(4))
