330536577ced330b49813bc9c2c54e9232434853
GaoFan98 <musepsycho98@gmail.com>
2021-04-19 10:07:18 +0400

    kadanes algo upd simplified

--- Algo/largest_continuous_sum.py
+++ Algo/largest_continuous_sum.py
@@ -3,13 +3,13 @@ def large_cont_sum(arr):
     if len(arr) == 0:
         return 0
 
-    max_arr = curr_max = 0
+    max_current = max_global = arr[0]
 
-    for num in range(1, len(arr)):
-        curr_max = max(arr[num], curr_max + arr[num])
-        max_arr = max(max_arr, curr_max)
+    for num in arr[1:]:
+        max_current = max(num, max_current + num)
+        max_global = max(max_global, max_current)
 
-    return max_arr
+    return max_global
 
 
 out = large_cont_sum([-2, -3, 4, -1, -2, 1, 5, -3])
