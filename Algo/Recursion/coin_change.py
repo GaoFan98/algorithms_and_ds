d56a5f338d7c55bf19fa26bd1b3a7a6a9abb7731
GaoFan98 <musepsycho98@gmail.com>
2021-04-23 22:33:04 +0400

    coin change upd and tree added

--- Algo/Recursion/coin_change.py
+++ Algo/Recursion/coin_change.py
@@ -1,23 +1,23 @@
 # recursive version
-def coin_change(target, coins):
-    min_coins = target
-
-    if target in coins:
-        return 1
-
-    else:
-        # for every coin value that is <= target
-        for coin in [c for c in coins if c <= target]:
-            num_coins = 1 + coin_change(target - coin, coins)
-
-            if num_coins < min_coins:
-                min_coins = num_coins
-
-    return min_coins
-
-
-out = coin_change(10, [1, 5, 10])
-print(out)
+# def coin_change(target, coins):
+#     min_coins = target
+#
+#     if target in coins:
+#         return 1
+#
+#     else:
+#         # for every coin value that is <= target
+#         for coin in [c for c in coins if c <= target]:
+#             num_coins = 1 + coin_change(target - coin, coins)
+#
+#             if num_coins < min_coins:
+#                 min_coins = num_coins
+#
+#     return min_coins
+#
+#
+# out = coin_change(10, [1, 5, 10])
+# print(out)
 
 
 # dynamical programming
@@ -25,16 +25,17 @@ def get_change(given_coin, coins):
     if given_coin == 0:
         return 0
 
-    table = [0] + [given_coin + 1 for i in range(given_coin)]
+    table = [0] + [given_coin + 1 for _ in range(given_coin)]
     # print(table)
 
     for index in range(0, given_coin + 1):
         for coin in coins:
-            if index >= coin:
+            if coin <= index:
                 table[index] = min(table[index], table[index - coin] + 1)
+                # print(table)
 
     return table[index]
 
 
-out = get_change(10, [1, 5, 10])
+out = get_change(11, [1, 2, 5])
 print(out)
