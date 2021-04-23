6d584a2d1069f60d19de999b42bb194e345d6f72
GaoFan98 <musepsycho98@gmail.com>
2021-04-23 15:56:35 +0400

    permute string

--- Algo/Recursion/permute.py
+++ Algo/Recursion/permute.py
@@ -1,5 +1,34 @@
 def permute(s):
+    out = []
+
+    if len(s) == 1:
+        out = [s]
+
+    else:
+        for index, el in enumerate(s):
+            for perm in permute(s[:index] + s[index + 1:]):
+                out += [el + perm]
+
+    return out
 
 
 out = permute('abc')
+print(out)
+
+
+def permutations(head, tail=''):
+    if len(head) == 0:
+        print(tail)
+    else:
+        for i in range(len(head)):
+            # print(head[:i])
+            # print(head[i + 1:])
+            # print(head[i + 1:] + head[:i])
+            # print(head[i])
+            # print(tail)
+            # print(head[i + 1:] + head[:i], head[i] + tail)
+            permutations(head[:i] + head[i + 1:], head[i] + tail)
+
+
+out = permutations('abc')
 print(out)
