e48441fe535b58431dcca58429ae1891d32f558d
GaoFan98 <musepsycho98@gmail.com>
2021-04-24 19:04:35 +0400

    check valid bst

--- Algo/Trees/binary_search_tree_check.py
+++ Algo/Trees/binary_search_tree_check.py
@@ -1,6 +1,14 @@
-def binary_tree_check():
-    pass
+import math
 
 
-out = binary_tree_check()
-print(out)
+def isValidBST(node, min=-math.inf, max=math.inf):
+    # Empty trees are valid BSTs.
+    if node == None:
+        return True
+    # The current node's value must be between low and high.
+    if node.val <= min or node.val >= max:
+        return False
+    # The left and right subtree must also be valid.
+    return (isValidBST(node.left, min, node.val)
+            and
+            isValidBST(node.right, node.val, max))
