d4b89f2eb7f069a7c156fa75636e33c8dc853b12
GaoFan98 <musepsycho98@gmail.com>
2021-05-18 14:22:37 +0400

    interview 2 q 1 simple 5 sided dice roll

---
+++ Algo/Interviews/2/q_1.py
@@ -0,0 +1,19 @@
+from random import randint
+
+
+def dice7():
+    return randint(1, 7)
+
+
+def convert7to5():
+    roll = 7
+
+    while roll > 5:
+        roll = dice7()
+        print('dice7 produced a roll of ', roll)
+
+    print('Your final returned roll is below')
+    print(roll)
+
+
+convert7to5()
