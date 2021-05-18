263488fd74abd16ae1afcd695955f332bb45ad09
GaoFan98 <musepsycho98@gmail.com>
2021-05-18 12:53:06 +0400

    interview 1 q2 done

--- Algo/Interviews/1/q_2.py
+++ Algo/Interviews/1/q_2.py
@@ -1,12 +1,11 @@
 def multiply_others(nums):
     right = [1]
+
     # calc from right to left
     for i in range(len(nums) - 1, 0, -1):
         right.append(right[-1] * nums[i])
         # [1]*5 -> [1,5]
-
         # 5*4 -> [1,5,20]
-        
         # 20*3 -> [1,5,20,60]
 
     right = right[::-1]
@@ -14,6 +13,11 @@ def multiply_others(nums):
 
     left = 1
     # calc from left to right
+    # then we multiple every number in right array to the actual array like:
+    # 60 * [1]
+    # 20 * [2]
+    # 5 * [2*3]
+    # 1 * [2*3*4]
     for i in range(len(nums)):
         right[i] = right[i] * left
         left = left * nums[i]
